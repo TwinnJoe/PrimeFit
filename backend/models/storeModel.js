672ea976495e5df2e91feff8fdac99b733c2c483
Twinn Joe <90099256+TwinnJoe@users.noreply.github.com>
2025-06-28 04:19:25 +0200

    Update storeModel.js

--- backend/models/storeModel.js
+++ backend/models/storeModel.js
@@ -3,7 +3,11 @@ import pool from "../config/config.js";
 const AllProducts = async () => {
     let [rows] = await pool.query("SELECT * FROM store");
     return rows
-}
+}catch (err) {
+    console.error("❌ SQL ERROR in AllProducts:", err); // Log full SQL error
+    throw err; // Let the controller catch it
+  }
+};
 
 export {AllProducts}
 
