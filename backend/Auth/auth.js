a3f74a781638eb1cc1570073cc3a1dd307cea185
Twinn Joe <90099256+TwinnJoe@users.noreply.github.com>
2025-06-30 08:40:27 +0200

    Update auth.js

--- backend/Auth/auth.js
+++ backend/Auth/auth.js
@@ -1,6 +1,4 @@
 import jwt from 'jsonwebtoken';
-JWT_SECRET = 'fdd6733c6254ae9e10a2dd003741e0ae8b9f1a5726face4067c6a4d74d9a9d385df81db438ee3ff0ded74b9bd46fc44a426f22418262d20ff65cd4976d5e789b'
-
 export const authenticateToken = (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1]; // expects 'Bearer <token>'
 
